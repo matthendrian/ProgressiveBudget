e1968dd0a04d1c211515d4dfa2de12b8a708d452
matthendrian <83432764+matthendrian@users.noreply.github.com>
2021-10-09 21:13:43 -0400

    updating port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-/*
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MyBudget" || 3000, {
+  process.env.MONGODB_URI || "mongodb://localhost/MyBudget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   retryWrites: true,
@@ -25,16 +25,8 @@ mongoose.connect(
 });
 
 mongoose.Promise = global.Promise;
-*/
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MyBudget" ,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+
+
 // routes
 app.use(require("./routes/api.js"));
 /*
