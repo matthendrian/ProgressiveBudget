c70f9d6ee098d050034c9810590d5213e908d5df
matthendrian <83432764+matthendrian@users.noreply.github.com>
2021-10-09 20:56:25 -0400

    Applying Heroku Fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MyBudget", {
+  process.env.MONGODB_URI || "mongodb://localhost/MyBudget" || 3000, {
   useNewUrlParser: true,
   useFindAndModify: false,
   retryWrites: true,
@@ -26,6 +26,10 @@ mongoose.connect(
 mongoose.Promise = global.Promise;
 // routes
 app.use(require("./routes/api.js"));
+/*
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
+*/
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
